e4c91227579c78ae866c5a880cf72cff6bbd2571
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-04-28 10:52:07 +0700

    6

---
+++ class2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class persegi_panjang{
+    private:
+        int panjang;
+        int lebar;
+        int luas;
+    public:
+        void input_data(){
+            cout << "Masukkan panjang: ";
+            cin >> panjang;
+            cout << "Masukkan lebar: ";
+            cin >> lebar;
+        }
+        void hitung_luas(){
+            luas = panjang * lebar;
+        }
+        void output_data(){
+            cout << "Luas persegi panjang: " << luas << endl;
+        }
+}
