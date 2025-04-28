21230e688fbd7310f01705d8b964521eecb63fe3
Abinayanafaiq <abinayanafaiq550@gmail.com>
2025-04-28 10:45:36 +0700

    4

--- class.cpp
+++ class.cpp
@@ -5,4 +5,20 @@ class Mobil{
     public:
         string nama_mobil;
         string tipe_mobil;
-}
+    
+    private:
+        int harga_mobil;
+
+    public:
+        void input_data(){
+            cout << "Masukkan nama mobil: ";
+            cin >> nama_mobil;
+            cout << "Masukkan tipe mobil: ";
+            cin >> tipe_mobil;
+            cout << "Masukkan harga mobil: ";
+            cin >> harga_mobil;
+        }
+        void output_data(){
+            cout << "harga mobil: " << harga_mobil << endl;
+        }
+};
